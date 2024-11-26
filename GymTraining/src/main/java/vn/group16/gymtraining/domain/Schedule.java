d7e2fee355ec6cea3e6d74e938fa47b3221c0844
chubebanso <vuquanghoanglam@gmail.com>
2024-11-26 18:06:58 +0700

    get schedule by day

--- GymTraining/src/main/java/vn/group16/gymtraining/domain/Schedule.java
+++ GymTraining/src/main/java/vn/group16/gymtraining/domain/Schedule.java
@@ -1,27 +1,32 @@
 package vn.group16.gymtraining.domain;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.JoinColumn;
+
 @Entity
 @Table(name = "schedule")
 public class Schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String title;
     private String startTime;
     private String endTime;
     private LocalDate date;
-    @ManyToOne
-    @JoinColumn(name = "workout_id")
-    private Workout workout;
+    private String title;
+    @OneToMany(mappedBy = "schedule")
+    @JsonManagedReference
+    private List<Workout> workout;
 
     public long getId() {
         return id;
@@ -55,20 +60,20 @@ public class Schedule {
         this.date = date;
     }
 
+    public List<Workout> getWorkout() {
+        return workout;
+    }
+
+    public void setWorkout(List<Workout> workout) {
+        this.workout = workout;
+    }
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public Workout getWorkout() {
-        return workout;
-    }       
-
-    public void setWorkout(Workout workout) {
-        this.workout = workout;
     }
 
 }
