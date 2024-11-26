19213ed936cdd5a99711ea189c1e82f7541ca4b7
chubebanso <vuquanghoanglam@gmail.com>
2024-11-26 18:06:58 +0700

    get schedule by day

--- GymTraining/src/main/java/vn/group16/gymtraining/domain/Workout.java
+++ GymTraining/src/main/java/vn/group16/gymtraining/domain/Workout.java
@@ -1,11 +1,16 @@
 package vn.group16.gymtraining.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-@Entity 
+
+@Entity
 @Table(name = "workout")
 public class Workout {
     @Id
@@ -16,41 +21,66 @@ public class Workout {
     private String image;
     private int duration;
     private int calories;
-    
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    @JsonBackReference
+    Schedule schedule;
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getImage() {
         return image;
     }
+
     public void setImage(String image) {
         this.image = image;
     }
+
     public int getDuration() {
         return duration;
     }
+
     public void setDuration(int duration) {
         this.duration = duration;
     }
+
     public int getCalories() {
         return calories;
     }
+
     public void setCalories(int calories) {
         this.calories = calories;
     }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
 }
--- GymTraining/src/main/java/vn/group16/gymtraining/dto/EventDTO.java
+++ GymTraining/src/main/java/vn/group16/gymtraining/dto/EventDTO.java
@@ -5,9 +5,10 @@ public class EventDTO {
     public String start;
     public String end;
 
-    public EventDTO(String start, String end) {
+    public EventDTO(String title, String start, String end) {
         this.start = start;
         this.end = end;
+        this.title = title;
     }
 
 }
