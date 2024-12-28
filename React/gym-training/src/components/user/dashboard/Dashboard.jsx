18965e3452cd25118ee556a94d1a6fb20609c560
Taduc2003 <buitaduc139@gmail.com>
2024-12-28 09:42:35 +0700

    viết api tính calo cho home, api sơ đồ cho homepage

--- React/gym-training/src/components/user/dashboard/Dashboard.jsx
+++ React/gym-training/src/components/user/dashboard/Dashboard.jsx
@@ -1,50 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import "./dashboard.css";
-//import WorkoutCard from "./WorkoutCard";
+import WorkoutList from "./WorkoutList";
 import MonthlyWorkoutComparison from "./MonthlyWorkoutComparison";
 import KcalBurnOverview from "./KcalBurnOverview";
 
-const WorkoutCard = ({ workout }) => {
-  return (
-      <div className="workout-card">
-          <p className="workout-time">Today</p>
-          <p>Duration: {workout.duration}</p>
-          <p>Workout details: {workout.description}</p>
-      </div>
-  );
-};
+
 
 const Dashboard = () => {
-  const [workoutData, setWorkoutData] = useState([]);
-  const [showAll, setShowAll] = useState(false);
-  const accessToken = localStorage.getItem("accessToken");
-
-  const getVietnamDate = () => {
-    const vietnamTimeOffset = 7 * 60; // Vietnam is UTC+7
-    const localDate = new Date();
-    const utcDate = new Date(localDate.getTime() + localDate.getTimezoneOffset() * 60000);
-    const vietnamDate = new Date(utcDate.getTime() + vietnamTimeOffset * 60000);
-    return vietnamDate.toLocaleDateString('en-CA'); // Format as YYYY-MM-DD
-  };
-
-  useEffect(() => {
-    const fetchWorkoutData = async () => {
-      try {
-        const date = getVietnamDate();
-        const response = await fetch(`http://localhost:8080/api/v1/workouts/recent-workout?date=${date}`, {
-          headers: {
-            'Authorization': `Bearer ${accessToken}`,
-          }
-        });
-        const data = await response.json();
-        setWorkoutData(data.data);
-      } catch (error) {
-        console.error('Error fetching workout data:', error);
-      }
-    };
-
-    fetchWorkoutData();
-  }, []);
 
   return (
     <div className="home">
@@ -65,14 +27,7 @@ const Dashboard = () => {
       </div>
       <div className="home-list">
         <div className="home-list-workout">
-          <div>Upcoming Workouts
-            {workoutData.slice(0, showAll ? workoutData.length : 3).map((workout, index) => (
-              <WorkoutCard key={index} workout={workout} />
-            ))}
-            <div className="view-all">
-              <a onClick={() => setShowAll(!showAll)}>{showAll ? 'Show less' : 'View all'}</a>
-            </div>
-          </div>
+            <WorkoutList />
         </div>
         <div className="home-list-achievement">
           <div>My Achievements
